0090393c520fd5a4db9736178494e2a9028ff2ce
bogdan-constantin13 <bogdan.constantin.it@gmail.com>
2022-11-03 22:05:19 +0200

    TA-Setup3 Curs 03.11.2022

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,24 @@ export default function Home() {
     <div>
       Hello Boston <i className="fa-solid fa-house"></i>
       <img src="/images/logo.jpg" alt="Alt"></img>
+      <button className="button button-primary">Button Primary</button>
+      <button className="button button-primary button-large">
+        Button Primary Large
+      </button>
+      <a className="button button-primary">Button Primary</a>
+      <a className="button button-primary button-large">Button Primary Large</a>
+      <div className="mt-5">
+        <button className="button-large button button-primary">
+          Button Primary
+        </button>
+        <button className="button-large button button-primary button-large">
+          Button Primary Large
+        </button>
+        <a className="button-large button button-primary">Button Primary</a>
+        <a className="button-large button button-primary button-large">
+          Button Primary Large
+        </a>
+      </div>
     </div>
   );
 }
