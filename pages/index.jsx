988185109ebe891cf46228da01105e17f0e57d54
bogdan-constantin13 <bogdan.constantin.it@gmail.com>
2022-11-08 22:16:58 +0200

    TA-Setup3 HeaderSecondary - Curs 08.11.2022

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,28 +1,15 @@
-// import Image from 'next/image';
+import Header from '../components/common/header/Header';
 
 export default function Home() {
   return (
     <div>
-      Hello Boston <i className="fa-solid fa-house"></i>
-      <img src="/images/logo.jpg" alt="Alt"></img>
-      <button className="button button-primary">Button Primary</button>
-      <button className="button button-primary button-large">
-        Button Primary Large
-      </button>
-      <a className="button button-primary">Button Primary</a>
-      <a className="button button-primary button-large">Button Primary Large</a>
-      <div className="mt-5">
-        <button className="button-large button button-primary">
-          Button Primary
-        </button>
-        <button className="button-large button button-primary button-large">
-          Button Primary Large
-        </button>
-        <a className="button-large button button-primary">Button Primary</a>
-        <a className="button-large button button-primary button-large">
-          Button Primary Large
-        </a>
-      </div>
+      <header className="header container mt-5">
+        <Header></Header>
+      </header>
+
+      <main className="content"></main>
+
+      <footer className="footer"></footer>
     </div>
   );
 }
