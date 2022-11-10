b23cfa5cb5db5ab1cf92af027aeb60ff7d08b01d
bogdan-constantin13 <bogdan.constantin.it@gmail.com>
2022-11-10 22:22:52 +0200

    TA-Setup5 Footer/Header - Curs 10.11.2022

---
+++ components/common/footer/FooterSecondary.jsx
@@ -0,0 +1,11 @@
+export default function FooterSecondary() {
+  return (
+    <>
+      <p className="mb-2">©2019 THREAD AFFAIR. All Rights Reserved.</p>
+      <p className="mb-2">
+        Designed By
+        <img src="" alt="" />
+      </p>
+    </>
+  );
+}
